c037cd0cc7d37cfbb9d84c74695546c632fd6dee
yousmaaza <dataforsciences@gmail.com>
2024-12-20 13:08:14 +0100

    fix: mise à jour du chemin d'import des styles globaux

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-// Importation des styles globaux
+// Import des styles globaux
 import './styles/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
