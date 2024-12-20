4cbda61899acf11919620b0f062e8788e43c5db0
yousmaaza <dataforsciences@gmail.com>
2024-12-20 12:39:28 +0100

    feat: ajout du layout principal

--- src/components/layouts/main-layout.tsx
+++ src/components/layouts/main-layout.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom'
-import { Navbar } from '@/components/layouts/navbar'
+import { Navbar } from './navbar'
 
 export function MainLayout() {
   return (
     <div className="min-h-screen bg-background">
       <Navbar />
-      <main className="container mx-auto py-6">
+      <main className="container mx-auto py-6 px-4">
         <Outlet />
       </main>
     </div>
